8bff0f915c251426282f8b7938bf601972b18f4a
Antoine Labard <antoine@labard.fr>
2022-01-18 17:37:34 +0100

    ✨ feat: ajout du bloc 'motor_move'
    Ce bloc permet de contrôler indépendamment chaque moteur du robot.

--- blockly/blocks/pirobot.js
+++ blockly/blocks/pirobot.js
@@ -13,18 +13,36 @@ Blockly.Blocks['robot_motor_power'] = {
 	}
 };
 
-Blockly.Blocks['control_key_down'] = {
-	init : function() {
-		this.setHelpUrl('http://www.example.com/');
-		this.setColour(210);
-		this.appendDummyInput().appendField(new Blockly.FieldDropdown([
-                        [ "On ↓ key", "DOWN" ],	[ "On ↑ key", "UP" ], 
-                        [ "On ← key", "LEFT" ],	[ "On → key", "RIGHT" ] ]), "KEY")
-            .appendField(new Blockly.FieldDropdown([ [ "up", "UP" ], [ "down", "DOWN" ] ]), "TYPE");
-        
-		this.appendStatementInput("STATEMENTS");
-		this.setTooltip('');
-	}
+Blockly.Blocks['motor_move'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("faire")
+            .appendField(new Blockly.FieldDropdown([["avancer","av"], ["reculer","re"]]), "MOVE")
+        this.appendValueInput("POWER2").setCheck("Number").appendField(
+            new Blockly.FieldDropdown([
+                [ "le moteur 0 de", "0" ],
+                [ "le moteur 1 de", "1" ],
+                [ "le moteur 2 de", "2" ],
+                [ "le moteur 3 de", "3" ],
+                [ "le moteur 4 de", "4" ],
+                [ "le moteur 5 de", "5" ],
+                [ "le moteur 6 de", "6" ],
+                [ "le moteur 7 de", "7" ],
+                [ "le moteur 8 de", "8" ],
+                [ "le moteur 9 de", "9" ],
+                [ "le moteur 10 de", "10" ],
+                [ "le moteur 11 de", "11" ],
+                [ "le moteur 12 de", "12" ],
+                [ "le moteur 13 de", "13" ],
+                [ "le moteur 14 de", "14" ],
+                [ "le moteur 15 de", "15" ]]),
+            "MOTOR2");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
 };
 
 Blockly.Blocks['block_drt'] = {
@@ -163,4 +181,18 @@ Blockly.Blocks['question_vocale'] = {
  this.setTooltip("Pose une question au robot");
  this.setHelpUrl("");
   }
+};
+
+Blockly.Blocks['control_key_down'] = {
+    init : function() {
+        this.setHelpUrl('http://www.example.com/');
+        this.setColour(210);
+        this.appendDummyInput().appendField(new Blockly.FieldDropdown([
+            [ "On ↓ key", "DOWN" ],	[ "On ↑ key", "UP" ],
+            [ "On ← key", "LEFT" ],	[ "On → key", "RIGHT" ] ]), "KEY")
+            .appendField(new Blockly.FieldDropdown([ [ "up", "UP" ], [ "down", "DOWN" ] ]), "TYPE");
+
+        this.appendStatementInput("STATEMENTS");
+        this.setTooltip('');
+    }
 };
