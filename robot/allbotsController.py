ff05411b53a7f3aa2876fc8d8c035a60ed32abcd
Antoine Labard <antoine@labard.fr>
2022-02-04 15:07:46 +0100

    feat: 🏗️ implémentation du module allbotsController.py sous forme de classe
    recyclage des fonctions en méthodes de la class AllbotsController.
    Ajustement des angles des moteurs en position initiale du robot.

--- robot/allbotsController.py
+++ robot/allbotsController.py
@@ -2,25 +2,31 @@ from adafruit_servokit import ServoKit
 from gpiozero import Motor
 from time import sleep
 
-kit = None
 
-# set the range of motion for each motor. The index of the list is the motor's number
-motors_range = [
-    (30, 150), (0, 90),
-    (30, 150), (0, 90),
-    (30, 150), (0, 90),
-    (30, 150), (0, 90)
-]
+class AllbotsController:
 
+    def reset_position(self):
+        self.servo_kit.servo[0].angle = 10
+        self.servo_kit.servo[1].angle = 30
+        self.servo_kit.servo[2].angle = 10
+        self.servo_kit.servo[3].angle = 300
+        self.servo_kit.servo[4].angle = 10
+        self.servo_kit.servo[5].angle = 150
+        self.servo_kit.servo[6].angle = 10
+        self.servo_kit.servo[7].angle = 150
 
-def init():
-    global kit
-    kit = ServoKit(channels=16)
-    kit.servo[0].angle = 30
-    kit.servo[1].angle = 0
-    kit.servo[2].angle = 30
-    kit.servo[3].angle = 0
-    kit.servo[4].angle = 30
-    kit.servo[5].angle = 0
-    kit.servo[6].angle = 30
-    kit.servo[7].angle = 0
+    def __init__(self):
+        # set the range of motion for each motor. The index of the list is the motor's number
+        self.MOTORS_RANGES = [
+            (30, 150), (0, 90),
+            (30, 150), (0, 90),
+            (30, 150), (0, 90),
+            (30, 150), (0, 90)
+        ]
+        self.MIN_IMP, self.MAX_IMP = 500, 2500
+
+        self.servo_kit = ServoKit(channels=16)
+        for i in range(len(self.MOTORS_RANGES)):
+            self.servo_kit.servo[i].set_pulse_width_range(self.MIN_IMP, self.MAX_IMP)
+
+        self.reset_position()
