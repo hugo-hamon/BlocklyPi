b15918ae559f165c2bad7539ae7f622c02ea6f05
Antoine Labard <antoine@labard.fr>
2022-02-04 17:19:33 +0100

    refactor: 🧑‍💻 déclaration de constantes pour les indices des moteurs
    Les moteurs ne sont désormais plus désignés dans le programme par leur indice sur la carte d'alimentation, mais par une variable dont le nom indique le mouvement qu'ils permettent au robot. En plus d'améliorer la lisiblité du code, celui-ci est factorisé et il n'y a plus qu'une variable à modifier pour changer l'indice d'un moteur.

--- robot/allbotsController.py
+++ robot/allbotsController.py
@@ -5,6 +5,16 @@ from time import sleep
 
 class AllbotsController:
 
+    # set of constants used to replace the motor numbers by more meaningful names
+    FRONT_LEFT_ELBOW = 0
+    FRONT_LEFT_SHOULDER = 1
+    FRONT_RIGHT_ELBOW = 2
+    FRONT_RIGHT_SHOULDER = 3
+    BACK_LEFT_ELBOW = 4
+    BACK_LEFT_SHOULDER = 5
+    BACK_RIGHT_ELBOW = 6
+    BACK_RIGHT_SHOULDER = 7
+
     # set the range of motion for each motor. The index of the list is the motor's number
     MOTORS_RANGES = [
         (30, 150), (0, 90),
@@ -15,14 +25,14 @@ class AllbotsController:
     MIN_IMP, MAX_IMP = 500, 2500
 
     def reset_position(self):
-        self.servo_kit.servo[0].angle = 10
-        self.servo_kit.servo[1].angle = 30
-        self.servo_kit.servo[2].angle = 10
-        self.servo_kit.servo[3].angle = 300
-        self.servo_kit.servo[4].angle = 10
-        self.servo_kit.servo[5].angle = 150
-        self.servo_kit.servo[6].angle = 10
-        self.servo_kit.servo[7].angle = 150
+        self.servo_kit.servo[self.FRONT_LEFT_ELBOW].angle = 10
+        self.servo_kit.servo[self.FRONT_LEFT_SHOULDER].angle = 30
+        self.servo_kit.servo[self.FRONT_RIGHT_ELBOW].angle = 10
+        self.servo_kit.servo[self.FRONT_RIGHT_SHOULDER].angle = 300
+        self.servo_kit.servo[self.BACK_LEFT_ELBOW].angle = 10
+        self.servo_kit.servo[self.BACK_LEFT_SHOULDER].angle = 150
+        self.servo_kit.servo[self.BACK_RIGHT_ELBOW].angle = 10
+        self.servo_kit.servo[self.BACK_RIGHT_SHOULDER].angle = 150
 
     def __init__(self):
 
--- robot/francasterController.py
+++ robot/francasterController.py
@@ -4,6 +4,25 @@ from time import sleep
 
 
 class FrancasterController:
+
+    # set of constants used to replace the motor numbers by more meaningful names
+    LEFT_ELBOW = 0
+    LEFT_SHOULDER_ABDUCTOR = 1
+    LEFT_SHOULDER_ROTATOR = 2
+    LEFT_SHOULDER_FLEXOR = 3
+    RIGHT_SHOULDER_FLEXOR = 4
+    RIGHT_SHOULDER_ABDUCTOR = 5
+    RIGHT_SHOULDER_ROTATOR = 6
+    RIGHT_ELBOW = 7
+    LEFT_HIP = 8
+    LEFT_KNEE = 9
+    LEFT_ANKLE = 10
+    RIGHT_HIP = 11
+    RIGHT_KNEE = 12
+    RIGHT_ANKLE = 13
+    HEAD_YAW = 14
+    HEAD_PITCH = 15
+
     # set the range of motion for each motor. The index of the list is the motor's number
     MOTORS_RANGES = [(0, 180), (0, 180), (0, 180), (0, 180), (0, 180), (0, 180), (0, 180), (0, 170), (0, 180),
                      (0, 135), (0, 120), (0, 180), (45, 180), (0, 120), (0, 180), (35, 140)]
@@ -12,21 +31,21 @@ class FrancasterController:
 
     def reset_position(self):
         self.servo_kit.servo[0].angle = 180
-        self.servo_kit.servo[1].angle = 90
-        self.servo_kit.servo[2].angle = 180
-        self.servo_kit.servo[3].angle = 90
-        self.servo_kit.servo[4].angle = 90
-        self.servo_kit.servo[5].angle = 0
-        self.servo_kit.servo[6].angle = 90
-        self.servo_kit.servo[7].angle = 0
-        self.servo_kit.servo[8].angle = 90
-        self.servo_kit.servo[9].angle = 0
-        self.servo_kit.servo[10].angle = 45
-        self.servo_kit.servo[11].angle = 90
-        self.servo_kit.servo[12].angle = 180
-        self.servo_kit.servo[13].angle = 75
-        self.servo_kit.servo[14].angle = 90
-        self.servo_kit.servo[15].angle = 90
+        self.servo_kit.servo[self.LEFT_SHOULDER_ABDUCTOR].angle = 90
+        self.servo_kit.servo[self.LEFT_SHOULDER_ROTATOR].angle = 180
+        self.servo_kit.servo[self.LEFT_SHOULDER_FLEXOR].angle = 90
+        self.servo_kit.servo[self.RIGHT_SHOULDER_FLEXOR].angle = 90
+        self.servo_kit.servo[self.RIGHT_SHOULDER_ABDUCTOR].angle = 0
+        self.servo_kit.servo[self.RIGHT_SHOULDER_ROTATOR].angle = 90
+        self.servo_kit.servo[self.RIGHT_ELBOW].angle = 0
+        self.servo_kit.servo[self.LEFT_HIP].angle = 90
+        self.servo_kit.servo[self.LEFT_KNEE].angle = 0
+        self.servo_kit.servo[self.LEFT_ANKLE].angle = 45
+        self.servo_kit.servo[self.RIGHT_HIP].angle = 90
+        self.servo_kit.servo[self.RIGHT_KNEE].angle = 180
+        self.servo_kit.servo[self.RIGHT_ANKLE].angle = 75
+        self.servo_kit.servo[self.HEAD_YAW].angle = 90
+        self.servo_kit.servo[self.HEAD_PITCH].angle = 90
 
     def __init__(self):
         self.reset_position()
@@ -75,84 +94,84 @@ class FrancasterController:
 
     def walk_n_steps(self, n):
         for _ in range(0, n - 1):
-            self.servo_kit.servo[3].angle = 60
+            self.servo_kit.servo[self.LEFT_SHOULDER_FLEXOR].angle = 60
             sleep(.1)
-            self.servo_kit.servo[4].angle = 60
+            self.servo_kit.servo[self.RIGHT_SHOULDER_FLEXOR].angle = 60
             sleep(.1)
-            self.servo_kit.servo[8].angle = 120
+            self.servo_kit.servo[self.LEFT_HIP].angle = 120
             sleep(.1)
-            self.servo_kit.servo[11].angle = 120
+            self.servo_kit.servo[self.RIGHT_HIP].angle = 120
             sleep(0.5)
-            self.servo_kit.servo[3].angle = 120
+            self.servo_kit.servo[self.LEFT_SHOULDER_FLEXOR].angle = 120
             sleep(.1)
-            self.servo_kit.servo[4].angle = 120
+            self.servo_kit.servo[self.RIGHT_SHOULDER_FLEXOR].angle = 120
             sleep(.1)
-            self.servo_kit.servo[8].angle = 60
+            self.servo_kit.servo[self.LEFT_HIP].angle = 60
             sleep(.1)
-            self.servo_kit.servo[11].angle = 60
+            self.servo_kit.servo[self.RIGHT_HIP].angle = 60
             sleep(.5)
 
     def do_hi(self, ps):
-        self.servo_kit.servo[5].angle = 130
+        self.servo_kit.servo[self.RIGHT_SHOULDER_ABDUCTOR].angle = 130
         sleep(.1)
-        self.servo_kit.servo[6].angle = 180
+        self.servo_kit.servo[self.RIGHT_SHOULDER_ROTATOR].angle = 180
         sleep(.1)
         for _ in range(0, ps - 1):
-            self.servo_kit.servo[7].angle = 90
+            self.servo_kit.servo[self.RIGHT_ELBOW].angle = 90
             sleep(0.5)
-            self.servo_kit.servo[7].angle = 0
+            self.servo_kit.servo[self.RIGHT_ELBOW].angle = 0
             sleep(0.5)
 
     def walk_n_steps_with_knee_lift(self, ps):
-        self.servo_kit.servo[2].angle = 170
-        self.servo_kit.servo[5].angle = 10
+        self.servo_kit.servo[self.LEFT_SHOULDER_ROTATOR].angle = 170
+        self.servo_kit.servo[self.RIGHT_SHOULDER_ABDUCTOR].angle = 10
         for _ in range(0, ps - 1):
-            self.servo_kit.servo[0].angle = 150
+            self.servo_kit.servo[self.LEFT_ELBOW].angle = 150
             sleep(0.2)
-            self.servo_kit.servo[3].angle = 90
+            self.servo_kit.servo[self.LEFT_SHOULDER_FLEXOR].angle = 90
             sleep(0.2)
-            self.servo_kit.servo[4].angle = 60
+            self.servo_kit.servo[self.RIGHT_SHOULDER_FLEXOR].angle = 60
             sleep(0.2)
-            self.servo_kit.servo[7].angle = 0
+            self.servo_kit.servo[self.RIGHT_ELBOW].angle = 0
             sleep(0.2)
-            self.servo_kit.servo[8].angle = 100
+            self.servo_kit.servo[self.LEFT_HIP].angle = 100
             sleep(0.2)
-            self.servo_kit.servo[9].angle = 60
+            self.servo_kit.servo[self.LEFT_KNEE].angle = 60
             sleep(0.2)
-            self.servo_kit.servo[11].angle = 120
+            self.servo_kit.servo[self.RIGHT_HIP].angle = 120
             sleep(0.2)
-            self.servo_kit.servo[12].angle = 180
+            self.servo_kit.servo[self.RIGHT_KNEE].angle = 180
             sleep(0.2)
-            self.servo_kit.servo[0].angle = 180
+            self.servo_kit.servo[self.LEFT_ELBOW].angle = 180
             sleep(0.2)
-            self.servo_kit.servo[3].angle = 120
+            self.servo_kit.servo[self.LEFT_SHOULDER_FLEXOR].angle = 120
             sleep(0.2)
-            self.servo_kit.servo[4].angle = 90
+            self.servo_kit.servo[self.RIGHT_SHOULDER_FLEXOR].angle = 90
             sleep(0.2)
-            self.servo_kit.servo[7].angle = 30
+            self.servo_kit.servo[self.RIGHT_ELBOW].angle = 30
             sleep(0.2)
-            self.servo_kit.servo[8].angle = 50
+            self.servo_kit.servo[self.LEFT_HIP].angle = 50
             sleep(0.2)
-            self.servo_kit.servo[9].angle = 0
+            self.servo_kit.servo[self.LEFT_KNEE].angle = 0
             sleep(0.2)
-            self.servo_kit.servo[11].angle = 80
+            self.servo_kit.servo[self.RIGHT_HIP].angle = 80
             sleep(0.2)
-            self.servo_kit.servo[12].angle = 120
+            self.servo_kit.servo[self.RIGHT_KNEE].angle = 120
             sleep(0.2)
 
     def do_no(self):
         for _ in range(0, 2):
             sleep(.5)
-            self.servo_kit.servo[14].angle = 120
+            self.servo_kit.servo[self.HEAD_YAW].angle = 120
             sleep(.5)
-            self.servo_kit.servo[14].angle = 60
+            self.servo_kit.servo[self.HEAD_YAW].angle = 60
 
     def do_yes(self):
         for _ in range(0, 2):
             sleep(.5)
-            self.servo_kit.servo[15].angle = 45
+            self.servo_kit.servo[self.HEAD_PITCH].angle = 45
             sleep(.5)
-            self.servo_kit.servo[15].angle = 90
+            self.servo_kit.servo[self.HEAD_PITCH].angle = 90
 
     def motor_dc(self, speed=1):
         motor = Motor(pinA, pinB)
