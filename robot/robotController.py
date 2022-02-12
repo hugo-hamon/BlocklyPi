386eeba1b18eb8ac4ee1c1f1e9c73dc39bbbbd63
Karsten Daemen <karsten.daemen@gmail.com>
2022-02-12 14:01:15 +0100

    refactor: 🧱 intégration de RobotMotor dans RobotController
    - Modification de la signature de la fonction is_in_range pour la rendre
    plus adaptée à l'utilisation de la classe RobotMotor.
    - Modification du
    code pour manipuler des instances de RobotMotor à la place de données
    disparates.

--- robot/robotController.py
+++ robot/robotController.py
@@ -1,16 +1,11 @@
 from adafruit_servokit import ServoKit
+from robot.robotMotor import RobotMotor
 
 """
 robotController implements the common methods on which every robot is based. any robot controller should inherit
 from this class. It defines the general constants, the constants associated with the motors number should be defined
 in the robot controller class.
 """
-
-# Indicate the range of motion of each motor in the robot.
-# - The keys are the motors numbers. Prefer to use class constants to name the motors for readability and
-#     maintainability.
-# - The values are a tuple of ints containing the minimum and the maximum angles of the range.
-MOTORS_RANGES = {}
 
 # constants used for the is_in_range function
 IN_RANGE = 0
@@ -20,43 +15,41 @@ ABOVE_RANGE = 1
 servo_kit = ServoKit(channels=16)
 
 
-def is_in_range(angle: int, angle_range: (int, int)):
+def is_in_range(angle: int, motor: RobotMotor):
     """
     @:param
         - motor_nb: the motor number.
         - angle: the set-point angle.
-        - range: a tuple containing the minimum and maximum angle of the motor.
+        - motor: the motor tested.
     @:return
         - IN_RANGE if angle is within the value provided by angle is between the values provided by range.
         - BELOW_RANGE if it is below the minimum value.
         - ABOVE_RANGE if it is above the maximum value.
     """
-    if angle_range[0] <= angle <= angle_range[1]:
+    if motor.min_angle <= angle <= motor.max_angle:
         return IN_RANGE
-    elif angle_range[0] > angle:
+    elif motor.min_angle > angle:
         return BELOW_RANGE
     else:
         return ABOVE_RANGE
 
 
-def set_motor_position(motor_nb: int, angle: int):
-    mi, ma = MOTORS_RANGES[motor_nb]
-    in_range = is_in_range(angle, MOTORS_RANGES[motor_nb])
+def set_motor_position(motor: RobotMotor, angle: int):
+    in_range = is_in_range(angle, motor)
     if in_range == 0:
-        servo_kit.servo[motor_nb].angle = angle
+        servo_kit.servo[motor.id].angle = angle
     elif in_range == -1:
-        servo_kit.servo[motor_nb].angle = mi
+        servo_kit.servo[motor.id].angle = motor.min_angle
     elif in_range == 1:
-        servo_kit.servo[motor_nb].angle = ma
+        servo_kit.servo[motor.id].angle = motor.max_angle
 
 
-def shift_motor_position(motor_nb: int, angle: int):
-    shifted_angle = servo_kit.servo[motor_nb].angle + angle
-    mi, ma = MOTORS_RANGES[motor_nb]
-    in_range = is_in_range(shifted_angle, MOTORS_RANGES[motor_nb])
+def shift_motor_position(motor: RobotMotor, angle: int):
+    shifted_angle = servo_kit.servo[motor.id].angle + angle
+    in_range = is_in_range(shifted_angle, motor)
     if in_range == 0:
-        servo_kit.servo[motor_nb].angle += angle
+        servo_kit.servo[motor.id].angle += angle
     elif in_range == -1:
-        servo_kit.servo[motor_nb].angle = mi
+        servo_kit.servo[motor.id].angle = motor.min_angle
     elif in_range == 1:
-        servo_kit.servo[motor_nb].angle = ma
+        servo_kit.servo[motor.id].angle = motor.max_angle
