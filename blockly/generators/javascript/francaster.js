0018de75a40ec09c17e9accc7026e07c2d1c5e90
Antoine Labard <antoine@labard.fr>
2022-02-22 12:10:26 +0100

    refactor: 👔 suppression de la logique métier côté client
    Tentons de la gérer côté serveur.

--- blockly/generators/javascript/francaster.js
+++ blockly/generators/javascript/francaster.js
@@ -2,17 +2,6 @@ Blockly.JavaScript['francaster_set_motor_power'] = function (block) {
     var value_power = Blockly.JavaScript.valueToCode(block, 'POWER', Blockly.JavaScript.ORDER_ATOMIC);
     var dropdown_motor = block.getFieldValue('MOTOR');
 
-
-    // Check the input ...
-    if (value_power > 100) {
-        alert("Maximum motor power for the robot is 100.");
-        return '';
-    }
-    if (value_power < -100) {
-        alert("Minimum motor power for the robot is 100.");
-        return '';
-    }
-
     // Generate the code ...
     return 'runPiRobotCommand("robotController.set_motor_position", "' + dropdown_motor + '",' + value_power + ');';
 };
@@ -21,22 +10,6 @@ Blockly.JavaScript['francaster_shift_motor_position'] = function (block) {
     var value_power = Blockly.JavaScript.valueToCode(block, 'POWER2', Blockly.JavaScript.ORDER_ATOMIC);
     var dropdown_motor = block.getFieldValue('MOTOR2');
     var move = block.getFieldValue('MOVE');
-    // Check the input ...
-    if (value_power > 180) {
-        alert("Maximum motor power for the robot is 180.");
-        return '';
-    }
-    if (value_power < 0) {
-        alert("Minimum motor power for the robot is 0.");
-        return '';
-    }
-    if (move === 're') {
-        value_power = value_power * -1;
-    }
-    if (dropdown_motor === 1) {
-        alert("l'angle est " + value_power + " voila!");
-        return '';
-    }
 
     return 'runPiRobotCommand("robotController.shift_motor_position", "' + dropdown_motor + '",' + value_power + ');';
 };
