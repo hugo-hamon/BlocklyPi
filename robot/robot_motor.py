0fd453a3f55380ca9d03624da4652155778aebaf
Arkazix <hugohamon27@gmail.com>
2023-04-19 14:34:03 +0200

    fix dropdown

--- robot/robot_motor.py
+++ robot/robot_motor.py
@@ -17,7 +17,7 @@ class RobotMotor:
         self.min_angle = min_angle
         self.max_angle = max_angle
         self.initial_angle = initial_angle
-        # self.servo_kit = ServoKit(channels=16)
+        self.servo_kit = ServoKit(channels=16)
 
     def is_in_range(self, angle: int) -> int:
         """Check if the given angle is in the range of the motor"""
@@ -28,32 +28,31 @@ class RobotMotor:
         else:
             return self.ABOVE_RANGE
 
-    
-    # def set_motor_position(self, angle: int) -> None:
-    #     """Set the motor position to the given angle"""
-    #     in_range = self.is_in_range(angle)
-    #     if in_range == 0:
-    #         self.servo_kit.servo[self.id].angle = angle
-    #     elif in_range == -1:
-    #         self.servo_kit.servo[self.id].angle = self.min_angle
-    #     elif in_range == 1:
-    #         self.servo_kit.servo[self.id].angle = self.max_angle
+    def set_motor_position(self, angle: int) -> None:
+        """Set the motor position to the given angle"""
+        in_range = self.is_in_range(angle)
+        if in_range == 0:
+            self.servo_kit.servo[self.id].angle = angle
+        elif in_range == -1:
+            self.servo_kit.servo[self.id].angle = self.min_angle
+        elif in_range == 1:
+            self.servo_kit.servo[self.id].angle = self.max_angle
 
-    # def shift_motor_position(self, angle: int) -> None:
-    #     """Shift the motor position by the given angle"""
-    #     servo_angle = self.servo_kit.servo[self.id].angle
-    #     if servo_angle is None:
-    #         logging.warning("Servo is not initialized")
-    #         return
-    #     shifted_angle = servo_angle + angle
-    #     in_range = self.is_in_range(round(shifted_angle))
-    #     if in_range == 0:
-    #         servo_angle += angle
-    #     elif in_range == -1:
-    #         servo_angle = self.min_angle
-    #     elif in_range == 1:
-    #         servo_angle = self.max_angle
-    #     self.servo_kit.servo[self.id].angle = round(servo_angle)
+    def shift_motor_position(self, angle: int) -> None:
+        """Shift the motor position by the given angle"""
+        servo_angle = self.servo_kit.servo[self.id].angle
+        if servo_angle is None:
+            logging.warning("Servo is not initialized")
+            return
+        shifted_angle = servo_angle + angle
+        in_range = self.is_in_range(round(shifted_angle))
+        if in_range == 0:
+            servo_angle += angle
+        elif in_range == -1:
+            servo_angle = self.min_angle
+        elif in_range == 1:
+            servo_angle = self.max_angle
+        self.servo_kit.servo[self.id].angle = round(servo_angle)
 
     def reset(self) -> None:
         """Reset the motor position to the initial angle"""
